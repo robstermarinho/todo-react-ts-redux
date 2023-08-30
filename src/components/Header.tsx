9613f22816c91c35876040262db2c9fd40c89e4f
Robert Marinho <robstermarinho@gmail.com>
2023-08-30 16:37:57 -0300

    Include link to the active in progress todo in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,22 @@ import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import {
   selectActiveCycleId,
+  selectActiveTaskId,
   selectNumberOfPreviousCycles,
 } from '../redux/reducers/cycleSlice'
-import { selectInfo } from '../redux/reducers/todoSlice'
+import {
+  selectActiveTodoByTaskId,
+  selectInfo,
+} from '../redux/reducers/todoSlice'
+import { reducerStateType } from '../redux/store'
 
 export function Header() {
   const info = useSelector(selectInfo)
   const numberOfPreviousCycles = useSelector(selectNumberOfPreviousCycles)
-  const activeCycleId = useSelector(selectActiveCycleId)
-
+  const activeTaskId = useSelector(selectActiveTaskId)
+  const activeTodo = useSelector((state: reducerStateType) =>
+    selectActiveTodoByTaskId(state, activeTaskId),
+  )
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
@@ -30,20 +37,24 @@ export function Header() {
           </li>
           <li>
             {' '}
-            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/blog">Blog (React Context)</NavLink>
+          </li>
+          <li>
+            {' '}
+            <NavLink to="/redux/blog">Blog (Redux AsyncThunk)</NavLink>
           </li>
         </ul>
 
         <div className={styles.infoContainer}>
           <Info
-            title="All Todos"
+            title="Todos"
             amount={`${info.totalCompleted} of ${info.total}`}
             numberHighlight={
               (info.totalCompleted === info.total && info.total > 0) || false
             }
           />
           <Info
-            title="All Tasks"
+            title="Tasks"
             amount={`${info.totalTasksCompleted} of ${info.totalTasks}`}
             purple
             numberHighlight={
@@ -53,14 +64,16 @@ export function Header() {
             }
           />
           <Info title="Cycles" amount={`${numberOfPreviousCycles}`} />
-          {activeCycleId && (
-            <Info
-              blink
-              purple
-              numberHighlight
-              title="In progress"
-              amount={`1`}
-            />
+          {activeTodo && (
+            <NavLink to={`/todo/${activeTodo.slug}`}>
+              <Info
+                blink
+                purple
+                numberHighlight
+                title="In progress"
+                amount={`1`}
+              />
+            </NavLink>
           )}
         </div>
       </div>
