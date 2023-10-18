0442dce1a2222d784303de15e28f8cfb790ffff3
Robert Marinho <robstermarinho@gmail.com>
2023-10-18 14:23:05 -0300

    Add .env

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'http://localhost:3333'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
